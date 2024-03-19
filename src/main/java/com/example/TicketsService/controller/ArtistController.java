e5b9c631ad609729339a534f8df9c27d8111a3e2
DuklasW <102741731+DuklasW@users.noreply.github.com>
2024-03-19 16:07:43 +0100

    test

--- src/main/java/com/example/TicketsService/controller/ArtistController.java
+++ src/main/java/com/example/TicketsService/controller/ArtistController.java
@@ -9,13 +9,11 @@ import com.example.TicketsService.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
